0476cae6265dd86588973a46dc455fd12b1acd5a
Adam Dipinto <adam.dipinto@gmail.com>
2019-03-06 07:05:49 -0800

    Import Contacts rather than Contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -10,11 +10,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoe#gmail.com"
-            phone="222-222-2222"
-          />
+          <Contacts />
         </div>
       </div>
     );
